3dc7b66ab56dd6a93350b2d8a3809b80f0058c48
Harish Krishna <harish.krsn@gmail.com>
2015-05-15 10:59:19 -0400

    using tangram style switcher leaflet plugin

--- js/demo.js
+++ js/demo.js
@@ -13,7 +13,7 @@ var hash = new L.Hash(map);
 new L.Control.Zoom({ position: 'topright' }).addTo(map);
 L.control.locate({ position: 'topright', keepCurrentZoomLevel: true }).addTo(map);
 L.control.locations({ position: 'topright', keepCurrentZoomLevel: true }).addTo(map);
-L.control.styles({ position: 'topright', keepCurrentZoomLevel: true }).addTo(map);
+L.control.styles({ scene: layer.scene }).addTo(map);
 
 // add pelias geocoder control
 L.control.geocoder({ position: 'topright' }).addTo(map);
